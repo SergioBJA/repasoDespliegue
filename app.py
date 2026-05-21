b2ab9b9aeba393f7d66c6ae428c2efee122c5894
sergiobja <sergiobohorquezj@gmail.com>
2026-05-21 12:43:27 +0200

    Añadimos codigo Python para probar el linter

---
+++ app.py
@@ -0,0 +1,9 @@
+""" Docstring del módulo """
+
+
+def suma(a: int, b: int) -> int:
+ """ docstring function
+ # debe dar errores en linter
+ """
+    x = "esta variable no se usa para nada"
+    return a+ b
