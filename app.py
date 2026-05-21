e1843128f65ea06e85990a4baac910f2bcb30966
SergioBJA <sergiobohorquezj@gmail.com>
2026-05-21 13:15:44 +0200

    Update module and function docstrings in app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-"""Módulo de operaciones matemáticas para el examen."""
+"""Modulo de operaciones matematicas."""
 
 
 def suma(a: int, b: int) -> int:
-    """Calcula la suma de dos números enteros."""
+    """Calcula la suma."""
     return a + b
